1c65d6bbd8b27bd72704a5cd47500e656fe9ec6b
seyeon1202 <162389954+seyeon1202@users.noreply.github.com>
2024-03-27 00:48:23 +0000

    second - 2024-03-27

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-x=st.slider('Select a value')
-st.write(x,'is a square',x*x)
+rand=np.random.normal(1,2,size=20)
+fig,ax=plt.subplots()
+ax.hist(rand,bins=15)
+st.pyplot(fig)
